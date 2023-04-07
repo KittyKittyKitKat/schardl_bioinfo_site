87b6a5d465d74410f6d24cdc240d39f5183903f7
KittyKittyKitKat <simon.palmer03@gmail.com>
2023-04-07 11:46:05 -0400

    Remove bad type hint

--- bioinfo_site/errors/handlers.py
+++ bioinfo_site/errors/handlers.py
@@ -3,5 +3,5 @@ from flask import Blueprint, render_template
 errors = Blueprint('errors', __name__)
 
 @errors.app_errorhandler(404)
-def error_404(error: HTTPException):
+def error_404(error):
     return render_template('errors/404.html'), 404
