ebf1744dd4338bc724d0d8fa285236b385c557d9
KittyKittyKitKat <simon.palmer03@gmail.com>
2023-04-07 11:41:46 -0400

    Page updates

--- bioinfo_site/routes/routes.py
+++ bioinfo_site/routes/routes.py
@@ -1,11 +1,18 @@
-from flask import render_template, Blueprint
+from pathlib import Path
+from flask import render_template, Blueprint, url_for
 
 routes = Blueprint('routes', __name__)
 
 @routes.route('/')
 @routes.route('/home')
 def home():
-    return render_template('home.html', title='Home')
+    slideshow_images = []
+    slideshow_images_path = Path(__name__.split('.')[0]) / 'static' / 'images' / 'slideshow'
+    for image_file in slideshow_images_path.glob('*'):
+        static_filename = str(Path(*image_file.parts[1:]))
+        aspect_ratio = 'is-4by3' if image_file.stem.endswith('_l') else 'is-3by4'
+        slideshow_images.append((static_filename, aspect_ratio))
+    return render_template('home.html', title='Home', images=slideshow_images)
 
 
 @routes.route('/project-details')
@@ -21,3 +28,8 @@ def biographies():
 @routes.route('/map')
 def map():
     return render_template('map.html', title='Map')
+
+
+@routes.route('/gbrowse')
+def gbrowse():
+    return render_template('gbrowse.html', title='GBrowse')
