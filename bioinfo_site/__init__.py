3181611468570f263d18cebc142187bc8654b8e9
KittyKittyKitKat <simon.palmer03@gmail.com>
2023-05-15 13:42:29 -0400

    Error handler updates

--- bioinfo_site/__init__.py
+++ bioinfo_site/__init__.py
@@ -5,6 +5,8 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
     from bioinfo_site.routes.routes import routes
+    from bioinfo_site.errors.handlers import errors
 
     app.register_blueprint(routes)
+    app.register_blueprint(errors)
     return app
--- bioinfo_site/errors/handlers.py
+++ bioinfo_site/errors/handlers.py
@@ -7,3 +7,7 @@ errors = Blueprint('errors', __name__)
 @errors.app_errorhandler(404)
 def error_404(error):
     return render_template('errors/404.html'), 404
+
+@errors.app_errorhandler(403)
+def error_403(error):
+    return render_template('errors/403.html'), 403
