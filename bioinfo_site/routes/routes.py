a5f0e81048f6f3abe2f4d3975629810e782cf23e
KittyKittyKitKat <simon.palmer03@gmail.com>
2023-09-05 16:17:02 -0400

    Remove old ecoregion page

--- bioinfo_site/routes/routes.py
+++ bioinfo_site/routes/routes.py
@@ -32,11 +32,6 @@ def biographies():
     return render_template('biographies.html', title='Biographies', bios=bios)
 
 
-@routes.route('/ecoregions')
-def ecoregions():
-    return render_template('ecoregions.html', title='Ecoregions')
-
-
 @routes.route('/collection-sites')
 def collection_sites():
     view = request.args.get('view')
