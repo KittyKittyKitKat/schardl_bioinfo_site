f3f9f55d2ebb2f84ea208bc4c78f4ac5d52ed8a1
KittyKittyKitKat <simon.palmer03@gmail.com>
2023-09-21 11:25:36 -0400

    Add additional HTTP Error handlers

--- bioinfo_site/errors/handlers.py
+++ bioinfo_site/errors/handlers.py
@@ -4,11 +4,21 @@ from flask import Blueprint, render_template
 errors = Blueprint('errors', __name__)
 
 
-@errors.app_errorhandler(404)
-def error_404(error):
-    return render_template('errors/404.html'), 404
+@errors.app_errorhandler(401)
+def error_401(error):
+    return render_template('errors/401.html'), 401
 
 
 @errors.app_errorhandler(403)
 def error_403(error):
     return render_template('errors/403.html'), 403
+
+
+@errors.app_errorhandler(404)
+def error_404(error):
+    return render_template('errors/404.html'), 404
+
+
+@errors.app_errorhandler(405)
+def error_405(error):
+    return render_template('errors/405.html'), 405
